115537e8bc70fbaefc203330c31842f7bf1c62de
Thiago.Novato <thiago.novato@OCB-SESCOOP.GO.LOCAL>
2018-10-15 13:54:15 -0300

    Testando o component Comments com Shallow Rendering

--- src/Comment.test.js
+++ src/Comment.test.js
@@ -2,15 +2,15 @@ import React from 'react'
 import Comment from './Comment'
 import { render } from 'enzyme'
 
-it('should renderv text', () => {
-    const comment = {
+it('should render text', () => {
+    const c = {
         comment: 'test'
     }
-    const wrapper = render(<Comment c={comment} />)
+    const wrapper = render(<Comment c={c} />)
     expect(wrapper.text()).toBe('Comentário: test')
 })
 
 it('should render empty', () => {
-    const wrapper = render(<Comment c={comment} />)
+    const wrapper = render(<Comment />)
     expect(wrapper.text()).toBe('Comentário: vazio')
 })
---
+++ src/Comments.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Comments from './Comments'
+import Comment from './Comment'
+
+describe('<Comments />', () => {
+    it('should render Comments', () => {
+        const comments = {
+            a: { id: 'a', comment: 'Comment 1' },
+            b: { id: 'b', comment: 'Comment 2' }
+        }
+        const wrapper = shallow(<Comments comments={comments} />)
+        expect(wrapper.find(Comment).length).toBe(2)
+
+        expect(wrapper.find(Comment).get(0).props.c).toBe(comments.a)
+
+        expect(wrapper.find(Comment).get(0).key).toBe(comments.a.id)
+    })
+    
+    it('should render with no Comments', () => {
+        const comments = {}
+        const wrapper = shallow(<Comments comments={comments} />)
+        expect(wrapper.find(Comment).length).toBe(0)
+    })
+})
